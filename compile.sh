679858e5e52ccee139870f141284456ddee5d32d
shuoyanhe <yanyanho@126.com>
2018-10-19 11:47:32 +0800

    [1.2.4] fix the compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+function LOG_ERROR()
+{
+    local content=${1}
+    echo -e "\033[31m"${content}"\033[0m"
+}
+
+function LOG_INFO()
+{
+    local content=${1}
+    echo -e "\033[32m"${content}"\033[0m"
+}
+
+function execute_cmd()
+{
+    local command="${1}"
+    eval ${command}
+    local ret=$?
+    if [ $ret -ne 0 ];then
+        LOG_ERROR "execute command ${command} FAILED"
+        exit 1
+    else
+        LOG_INFO "execute command ${command} SUCCESS"
+    fi
+}
+
 
 CUR_DIR=`pwd`
 
@@ -44,6 +69,7 @@ function install_deps_ubuntu()
 }
 
 
+
 function compile_sdk_client()
 {
     execute_cmd "./gradlew build"
@@ -55,6 +81,5 @@ if grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
 else
     install_deps_centos
 fi
-
 
 compile_sdk_client
