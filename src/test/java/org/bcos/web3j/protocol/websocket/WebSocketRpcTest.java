dad7c03a2109f93d476e78c0922664e11bd784f8
shuoyanhe <yanyanho@126.com>
2018-11-15 14:43:26 +0800

    add web3j websocket test

---
+++ src/test/java/org/bcos/web3j/protocol/websocket/WebSocketRpcTest.java
@@ -0,0 +1,37 @@
+package org.bcos.web3j.protocol.websocket;
+
+import org.bcos.web3j.protocol.core.Request;
+import org.bcos.web3j.protocol.core.methods.response.EthBlockNumber;
+import org.bcos.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
+public class WebSocketRpcTest {
+
+    private WebSocketClient webSocketClient  = new WebSocketClient(new URI("ws://127.0.0.1:8888/"));
+
+
+    private WebSocketService service = new WebSocketService(webSocketClient, true);
+
+    private Request<?, EthBlockNumber> request = new Request<>(
+            "blockNumber",
+            Arrays.asList("0"),
+            service,
+            EthBlockNumber.class);
+
+    public WebSocketRpcTest() throws URISyntaxException {
+    }
+
+
+    @Test
+    public void testConnectViaWebSocketClient() throws Exception {
+
+        service.connect();
+        service.sendAsync(request, Web3ClientVersion.class);
+
+        //  verify(webSocketClient).connectBlocking();
+    }
+}
