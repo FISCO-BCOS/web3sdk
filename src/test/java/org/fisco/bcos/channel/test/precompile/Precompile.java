282f9c985b6265d3699f14290a5926be2150f8e1
app <app@yujiechen_hust@163.com>
2018-11-22 13:48:32 +0800

    fix compile error related to UpdatePBFTNode

---
+++ src/test/java/org/fisco/bcos/channel/test/precompile/Precompile.java
@@ -0,0 +1,49 @@
+package org.fisco.bcos.channel.test;
+
+import org.fisco.bcos.channel.test.UpdatePBFTNode;
+import org.fisco.bcos.web3j.crypto.Credentials;
+import org.fisco.bcos.channel.client.Service;
+import org.fisco.bcos.web3j.protocol.Web3j;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.fisco.bcos.web3j.protocol.channel.ChannelEthereumService;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+class PrecompileManager
+{
+
+    //TODO: load from configuration
+    public static Credentials credentials = Credentials.create("b83261efa42895c38c6c2364ca878f43e77f3cddbc922bf57d0d48070f79feb6");
+    protected static Web3j web3j;
+
+    private static BigInteger gasPrice = new BigInteger("300000000");
+    private static BigInteger gasLimit = new BigInteger("300000000");
+    private static BigInteger initialWeiValue = new BigInteger("0");
+
+    public static void main(String[] args) throws Exception
+    {
+
+        /// init application context from xml
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        /// init service
+        Service service = context.getBean(Service.class);
+        service.run();
+        System.out.println("==== start ... ");
+        ChannelEthereumService channelEthereumService = new ChannelEthereumService();
+        channelEthereumService.setChannelService(service);
+        /// init web3j of specified group according to configuration file
+        web3j = Web3j.build(channelEthereumService, service.getGroupId());
+
+        if(args[1] == "pbft")
+        {
+            UpdatePBFTNode pbft = new UpdatePBFTNode();
+            pbft.call(args, web3j, credentials, service.getGroupId());
+        }
+        else
+        {
+            System.out.println("Invalid Param, only support updatePBFTNode precompile now!");
+        }
+    }
+}
--- src/test/java/org/fisco/bcos/channel/test/precompile/UpdatePBFTNode.java
+++ src/test/java/org/fisco/bcos/channel/test/precompile/UpdatePBFTNode.java
@@ -3,76 +3,60 @@ package org.fisco.bcos.channel.test;
 import org.fisco.bcos.channel.test.Miner;
 import org.fisco.bcos.web3j.crypto.Credentials;
 import org.fisco.bcos.web3j.protocol.Web3j;
-import org.fisco.bcos.web3j.protocol.core.DefaultBlockParameter;
-import org.fisco.bcos.web3j.protocol.core.methods.response.*;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.fisco.bcos.web3j.protocol.channel.ChannelEthereumService;
+///import org.fisco.bcos.web3j.protocol.core.DefaultBlockParameter;
+///import org.fisco.bcos.web3j.protocol.core.methods.response.*;
+///import org.springframework.context.support.ClassPathXmlApplicationContext;
+///import org.fisco.bcos.web3j.protocol.channel.ChannelEthereumService;
 
-import java.io.IOException;
 import java.math.BigInteger;
 
 public class UpdatePBFTNode
 {
-    //TODO: load from configuration
-    public Credentials credentials = Credentials.create("b83261efa42895c38c6c2364ca878f43e77f3cddbc922bf57d0d48070f79feb6");
-    protected static Web3j web3j;
-
     private static BigInteger gasPrice = new BigInteger("300000000");
     private static BigInteger gasLimit = new BigInteger("300000000");
     private static BigInteger initialWeiValue = new BigInteger("0");
 
-    public static void main(String[] args) throws Exception
+    public void call(String[] args, Web3j web3j, Credentials credentials, int groupId)
     {
-        /// init application context from xml
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-        /// init service
-        Service service = context.getBean(Service.class);
-        service.run();
-        System.out.println("==== start ... ");
-        ChannelEthereumService channelEthereumService = new ChannelEthereumService();
-        channelEthereumService.setChannelService(service);
-        /// init web3j of specified group according to configuration file
-        web3j = Web3j.build(channelEthereumService, service.getGroupId());
-
         /// get functions
         if(args.length < 1)
-            Usage();
-        String operation = args[1];
-        if(args.length < 2)
-            Usage();
-        String nodeId = args[2];
+            Usage(args);
+        String operation = args[2];
+        if(args.length < 3)
+            Usage(args);
+        String nodeId = args[3];
         if(operation == "add")
         {
-            System.out.println("==== add " + nodeId + " to PBFT leaders of " + service.getGroupId());
-            AddPBFTNode(nodeId);
-            System.out.println("==== add " + nodeId + " to PBFT leaders of " + service.getGroupId() + " END ====");
+            System.out.println("==== add " + nodeId + " to PBFT leaders of " + groupId);
+            AddPBFTNode(nodeId, web3j, credentials);
+            System.out.println("==== add " + nodeId + " to PBFT leaders of " + groupId + " END ====");
             System.exit(0);
         }
         if(operation == "remove")
         {
-            System.out.println("==== remove " + nodeId + " from PBFT leaders of " + service.getGroupId());
-            RemovePBFTNode(nodeId);
-            System.out.println("==== remove " + nodeId + " from PBFT leaders of " + service.getGroupId() + " END ====");
+            System.out.println("==== remove " + nodeId + " from PBFT leaders of " + groupId);
+            RemovePBFTNode(nodeId, web3j, credentials);
+            System.out.println("==== remove " + nodeId + " from PBFT leaders of " + groupId + " END ====");
             System.exit(0);
         }
     }
+
     private void Usage(String[] args)
     {
         System.out.println("Usage:");
-        System.out.println(args[0] + " add ${nodeId} : add given nodeId to PBFT leaders by calling precompile");
-        System.out.println(args[0] + " remove ${nodeId} : remove given nodeId from PBFT leaders by calling precompile");
+        System.out.println(args[0] + "pbft add ${nodeId} : add given nodeId to PBFT leaders by calling precompile");
+        System.out.println(args[0] + "pbft remove ${nodeId} : remove given nodeId from PBFT leaders by calling precompile");
         System.exit(0);
     }
 
-    private void RemovePBFTNode(String nodeId)
+    private void RemovePBFTNode(String nodeId, Web3j web3j, Credentials credentials)
     {
-        removeNode("0x1003", web3j, credentials, nodeId);
+        RemoveNode("0x1003", web3j, credentials, nodeId);
     }
 
-    private void AddPBFTNode(String nodeId)
+    private void AddPBFTNode(String nodeId, Web3j web3j, Credentials credentials)
     {
-        AddNode("0x1003", nodeId);
+        AddNode("0x1003", web3j, credentials, nodeId);
     }
 
     /**
@@ -85,7 +69,7 @@ public class UpdatePBFTNode
     private void RemoveNode(String address, Web3j web3j, Credentials credentials, String nodeId)
     {
         Miner miner = Miner.load(address, web3j, credentials, gasPrice, gasLimit);
-        miner.removePBFTNode(nodeId);
+        miner.remove(nodeId);
     }
 
     /**
@@ -98,7 +82,7 @@ public class UpdatePBFTNode
     private void AddNode(String address, Web3j web3j, Credentials credentials, String nodeId)
     {
         Miner miner = Miner.load(address, web3j, credentials, gasPrice, gasLimit);
-        miner.addPBFTNode(nodeId);
+        miner.add(nodeId);
     }
 }
 
