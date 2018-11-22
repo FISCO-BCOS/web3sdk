f4675895df3de9f5dca749045cd3057fe64ceb71
cyjseagull <1547158861@qq.com>
2018-11-23 00:03:45 +0800

    reformat code

--- src/test/java/org/fisco/bcos/channel/test/precompile/Precompile.java
+++ src/test/java/org/fisco/bcos/channel/test/precompile/Precompile.java
@@ -11,8 +11,7 @@ import org.fisco.bcos.web3j.protocol.channel.ChannelEthereumService;
 import java.io.IOException;
 import java.math.BigInteger;
 
-class PrecompileManager
-{
+class PrecompileManager {
 
     //TODO: load from configuration
     public static Credentials credentials = Credentials.create("b83261efa42895c38c6c2364ca878f43e77f3cddbc922bf57d0d48070f79feb6");
@@ -22,8 +21,7 @@ class PrecompileManager
     private static BigInteger gasLimit = new BigInteger("300000000");
     private static BigInteger initialWeiValue = new BigInteger("0");
 
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
 
         /// init application context from xml
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
@@ -35,13 +33,10 @@ class PrecompileManager
         channelEthereumService.setChannelService(service);
         /// init web3j of specified group according to configuration file
         web3j = Web3j.build(channelEthereumService, service.getGroupId());
-        if(args[0].equals("pbft"))
-        {
+        if (args[0].equals("pbft")) {
             UpdatePBFTNode pbft = new UpdatePBFTNode();
             pbft.call(args, web3j, credentials, service.getGroupId());
-        }
-        else
-        {
+        } else {
             System.out.println("Invalid Param, only support updatePBFTNode precompile now!");
         }
     }
