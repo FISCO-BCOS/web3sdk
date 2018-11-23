ab4e6d20f912b3c6223da750eef9ac9fbdafd434
mingzhenliu <mingzhenliu@webank.com>
2018-11-23 17:49:33 +0800

    Polling time is configurable

---
+++ src/main/java/org/bcos/web3j/utils/AttemptsConf.java
@@ -0,0 +1,30 @@
+package org.bcos.web3j.utils;
+
+/**
+ * Created by mingzhenliu on 2018/8/24.
+ */
+public class AttemptsConf {
+    public static Integer sleepDuration = 1500;
+    public static Integer attempts  = 40;
+    public AttemptsConf(int sleepDuration,int attempts){
+        this.sleepDuration = sleepDuration;
+        this.attempts = attempts;
+    }
+
+    public int getSleepDuration() {
+        return sleepDuration;
+    }
+
+    public void setSleepDuration(int sleepDuration) {
+        AttemptsConf.sleepDuration = sleepDuration;
+    }
+
+    public int getAttempts() {
+        return attempts;
+    }
+
+    public void setAttempts(int attempts) {
+        AttemptsConf.attempts = attempts;
+    }
+
+}
