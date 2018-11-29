4cb8bf20f9c086761091765aedb7ed0d104e1040
fqliao <fqliao@yeah.net>
2018-11-29 16:28:52 +0800

    deploy, send and call contract in console

--- src/test/java/org/fisco/bcos/web3j/console/ContractClassFactory.java
+++ src/test/java/org/fisco/bcos/web3j/console/ContractClassFactory.java
@@ -1,11 +1,54 @@
 package org.fisco.bcos.web3j.console;
 
-import org.fisco.bcos.web3j.tx.Contract;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.math.BigInteger;
 
 public class ContractClassFactory {
 	
 	public static Class<?>  getContractClass(String contractName) throws ClassNotFoundException, InstantiationException, IllegalAccessException
 	{
 		return (Class<?>) Class.forName(contractName); 
+	}
+	
+    public static Class[] getParameterType(Class clazz, String methodName) throws ClassNotFoundException {
+        Method[] methods = clazz.getDeclaredMethods();
+        Class[] type = null;
+        for (Method method : methods) {
+            if (methodName.equals(method.getName())) {
+                Parameter[] params = method.getParameters();
+                type =  new Class[params.length];
+                for(int i=0; i< params.length;i++) {
+                    type[i] = Class.forName(params[i].getParameterizedType().getTypeName());
+                }
+
+            }
+        }
+
+        return type;
+    }
+    
+	public static Object[] getPrametersObject(String[] params) {
+		Object[] argobj = new Object[params.length - 2];
+    	for (int i = 0; i < argobj.length; i++) 
+    	{	
+    		if("true".equals(params[i+2].trim()))
+    		{
+    			argobj[i] = true;
+    		}
+    		else if( "false".equals(params[i+2].trim()))
+    		{
+    			argobj[i] = false;
+    		}
+    		else if(params[i+2].matches("^[0-9]*$"))
+    		{
+    			argobj[i] = new BigInteger(params[i+2]);
+    		}
+       		else
+    		{
+       			argobj[i] = params[i+2];
+    		}
+		}
+		return argobj;
 	}
 }
