f4b98f062d7aa60ef01bab183d5eac4981918845
chaychen2005 <chaychen@webank.com>
2018-12-21 11:47:33 +0800

    Modify format

--- src/test/java/org/fisco/bcos/channel/test/precompile/PrecompileManager.java
+++ src/test/java/org/fisco/bcos/channel/test/precompile/PrecompileManager.java
@@ -33,7 +33,7 @@ class PrecompileManager {
         if (args[0].equals("pbft")) {
             UpdatePBFTNode pbft = new UpdatePBFTNode();
             pbft.call(args, web3j, credentials, service.getGroupId());
-		} else if (args[0].equals("setSystemConfig")) {
+        } else if (args[0].equals("setSystemConfig")) {
             SetSystemConfig config = new SetSystemConfig();
             config.call(args, web3j, credentials, service.getGroupId());
         } else {
