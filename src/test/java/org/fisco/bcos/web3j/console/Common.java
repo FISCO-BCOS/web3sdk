40da4406f20801c491ff27e9742ec546d59722c5
fqliao <fqliao@yeah.net>
2018-12-27 09:14:40 +0800

    add authority control for SystemConfig and CNS

---
+++ src/test/java/org/fisco/bcos/web3j/console/Common.java
@@ -0,0 +1,21 @@
+package org.fisco.bcos.web3j.console;
+
+public class Common {
+	
+	// system table for authority control
+	public static final String SYS_TABLES = "_sys_tables_";
+	public static final String SYS_MINERS = "_sys_miners_";
+	public static final String SYS_ACCESS_TABLE = "_sys_table_access_";
+	public static final String SYS_CONFIG = "_sys_config_";
+	public static final String SYS_CNS = "_sys_cns_";
+	
+	public static final String NOAUTHORITY = "This account is not authorized.";
+	
+	// test tx.origin for authority control
+	public static final String PRIVITEKEY1 = "3bed914595c159cbce70ec5fb6aff3d6797e0c5ee5a7a9224a21cae8932d84a4";
+	public static final String ORIGIN1 = "0xf1585b8d0e08a0a00fff662e24d67ba95a438256";
+	public static final String PRIVITEKEY2 = "ab40568a2f77b4cb70706b4c6119916a143eb75c0d618e5f69909af1f9f9695e";
+	public static final String ORIGIN2 = "0xc0d0e6ccc0b44c12196266548bec4a3616160e7d";
+	public static final String PRIVITEKEY3 = "d0fee0a4e3c545a9394965042f8f891b6e5482c212a7428ec175d6aed121353a";
+	public static final String ORIGIN3 = "0x1600e34312edea101d8b41a3465f2e381b66baed";
+}
