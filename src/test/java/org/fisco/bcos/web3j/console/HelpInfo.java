32cb7f35db639916df03148994c863b31c10e28f
fqliao <fqliao@yeah.net>
2019-01-09 11:37:46 +0800

    fix console space format and provide promptHelp for no params commands

--- src/test/java/org/fisco/bcos/web3j/console/HelpInfo.java
+++ src/test/java/org/fisco/bcos/web3j/console/HelpInfo.java
@@ -8,6 +8,124 @@ public class HelpInfo {
 		System.out.println();
 	}
 	
+	public static void helpNoParams(String func) 
+	{
+		switch (func) {
+		case "h":
+			help();
+			break;
+		case "gbn":
+			getBlockNumberHelp();
+			break;
+		case "gpv":
+			getPbftViewHelp();
+			break;
+		case "gol":
+			getObserverListHelp();
+			break;
+		case "gml":
+			getMinerListHelp();
+			break;
+		case "gcs":
+			getConsensusStatusHelp();
+			break;
+		case "gss":
+			getSyncStatusHelp();
+			break;
+		case "gcv":
+			getClientVersionHelp();
+			break;
+		case "gps":
+			getPeersHelp();
+			break;
+		case "ggp":
+			getGroupPeersHelp();
+			break;
+		case "ggl":
+			getGroupListHelp();
+			break;
+		case "gpt":
+			getPendingTransactionsHelp();
+			break;
+		case "gpts":
+			getPendingTxSizeHelp();
+			break;
+		case "gtc":
+			getTotalTransactionCountHelp();
+			break;
+
+		default:
+			break;
+		}
+		System.out.println();
+	}
+	
+	public static void help() 
+	{
+		System.out.println("Provide help information.");
+		System.out.println("Usage: h");
+	}
+	
+	public static void getBlockNumberHelp() 
+	{
+		System.out.println("Query the number of most recent block.");
+		System.out.println("Usage: gbn");
+	}
+	
+	public static void getPbftViewHelp() 
+	{
+		System.out.println("Query the pbft view of node.");
+		System.out.println("Usage: gpv");
+	}
+	
+	public static void getObserverListHelp() 
+	{
+		System.out.println("Query the pbft observer node.");
+		System.out.println("Usage: gol");
+	}
+	
+	public static void getMinerListHelp() 
+	{
+		System.out.println(" Query the pbft miner nodes.");
+		System.out.println("Usage: gml");
+	}
+	
+	public static void getConsensusStatusHelp() 
+	{
+		System.out.println("Query consensus status.");
+		System.out.println("Usage: gcs");
+	}
+	
+	public static void getSyncStatusHelp() 
+	{
+		System.out.println("Query sync status.");
+		System.out.println("Usage: gss");
+	}
+	
+	public static void getClientVersionHelp() 
+	{
+		System.out.println("Query the current client version.");
+		System.out.println("Usage: gcv");
+	}
+	
+	public static void getPeersHelp() 
+	{
+		System.out.println("Query peers currently connected to the client.");
+		System.out.println("Usage: gps");
+	}
+	
+	public static void getGroupPeersHelp() 
+	{
+		System.out.println("Query peers currently connected to the client in the specified group.");
+		System.out.println("Usage: ggp");
+	}
+	
+	public static void getGroupListHelp() 
+	{
+		System.out.println("Query group list.");
+		System.out.println("Usage: ggl");
+	}
+	
 	public static void getBlockByHashHelp()
 	{
 		System.out.println("Query information about a block by hash.");
@@ -65,7 +183,11 @@ public class HelpInfo {
 	{
 		System.out.println("Query pending transactions.");
 		System.out.println("Usage: gpt");
-		System.out.println();
+	}
+	public static void getPendingTxSizeHelp() 
+	{
+		System.out.println("Query pending transactions size.");
+		System.out.println("Usage: gpts");
 	}
 	public static void getCodeHelp() 
 	{
@@ -78,7 +200,6 @@ public class HelpInfo {
 	{
 		System.out.println("Query total transaction count.");
 		System.out.println("Usage: gtc");
-		System.out.println();
 	}
 	public static void deployHelp() 
 	{
