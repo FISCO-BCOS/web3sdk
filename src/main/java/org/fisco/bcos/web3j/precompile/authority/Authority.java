4d7f4d2390016306dae45e4b6f8f25e083149057
fqliao <fqliao@yeah.net>
2019-01-15 22:05:10 +0800

    optimize precompiled code

--- src/main/java/org/fisco/bcos/web3j/precompile/authority/Authority.java
+++ src/main/java/org/fisco/bcos/web3j/precompile/authority/Authority.java
@@ -1,34 +1,114 @@
 package org.fisco.bcos.web3j.precompile.authority;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+import org.fisco.bcos.web3j.abi.TypeReference;
+import org.fisco.bcos.web3j.abi.datatypes.Function;
+import org.fisco.bcos.web3j.abi.datatypes.Type;
+import org.fisco.bcos.web3j.abi.datatypes.Utf8String;
+import org.fisco.bcos.web3j.crypto.Credentials;
+import org.fisco.bcos.web3j.protocol.Web3j;
+import org.fisco.bcos.web3j.protocol.core.RemoteCall;
+import org.fisco.bcos.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.fisco.bcos.web3j.tx.Contract;
+import org.fisco.bcos.web3j.tx.TransactionManager;
+import org.fisco.bcos.web3j.tx.gas.ContractGasProvider;
 
-public class Authority {
-	
-	@JsonProperty("table_name")
-    private String tableName;
-    private String address;
-	@JsonProperty("enable_num")
-    private String enableNum;
-	
-	public String getTableName() {
-		return tableName;
-	}
-	public void setTableName(String tableName) {
-		this.tableName = tableName;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getEnableNum() {
-		return enableNum;
-	}
-	public void setEnableNum(String enableNum) {
-		this.enableNum = enableNum;
-	}
-    
-	
-    
+/**
+ * <p>Auto generated code.
+ * <p><strong>Do not modify!</strong>
+ * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
+ * or the org.fisco.bcos.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ *
+ * <p>Generated with web3j version none.
+ */
+public class Authority extends Contract {
+    private static final String BINARY = "";
+
+    public static final String FUNC_INSERT = "insert";
+
+    public static final String FUNC_QUERYBYNAME = "queryByName";
+
+    public static final String FUNC_REMOVE = "remove";
+
+    @Deprecated
+    protected Authority(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    protected Authority(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    @Deprecated
+    protected Authority(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    protected Authority(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public RemoteCall<TransactionReceipt> insert(String table_name, String addr) {
+        final Function function = new Function(
+                FUNC_INSERT, 
+                Arrays.<Type>asList(new Utf8String(table_name), 
+                new Utf8String(addr)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<String> queryByName(String table_name) {
+        final Function function = new Function(FUNC_QUERYBYNAME, 
+                Arrays.<Type>asList(new Utf8String(table_name)), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Utf8String>() {}));
+        return executeRemoteCallSingleValueReturn(function, String.class);
+    }
+
+    public RemoteCall<TransactionReceipt> remove(String table_name, String addr) {
+        final Function function = new Function(
+                FUNC_REMOVE, 
+                Arrays.<Type>asList(new Utf8String(table_name), 
+                new Utf8String(addr)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    @Deprecated
+    public static Authority load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return new Authority(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    @Deprecated
+    public static Authority load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new Authority(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public static Authority load(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        return new Authority(contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    public static Authority load(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        return new Authority(contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public static RemoteCall<Authority> deploy(Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        return deployRemoteCall(Authority.class, web3j, credentials, contractGasProvider, BINARY, "");
+    }
+
+    @Deprecated
+    public static RemoteCall<Authority> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(Authority.class, web3j, credentials, gasPrice, gasLimit, BINARY, "");
+    }
+
+    public static RemoteCall<Authority> deploy(Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        return deployRemoteCall(Authority.class, web3j, transactionManager, contractGasProvider, BINARY, "");
+    }
+
+    @Deprecated
+    public static RemoteCall<Authority> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(Authority.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
+    }
 }
