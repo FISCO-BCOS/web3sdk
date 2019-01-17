cb5cfefb63d0b9b0668bd709f21ef3802ef929b4
octopuswang <octopuswang@webank.com>
2019-01-17 10:59:31 +0800

    update DagTransferService

---
+++ src/main/java/org/fisco/bcos/web3j/precompile/dag2/DagTransferPerTest.java
@@ -0,0 +1,5 @@
+package org.fisco.bcos.web3j.precompile.dag2;
+
+public class DagTransferPerTest {
+
+}
--- src/main/java/org/fisco/bcos/web3j/precompile/dag2/DagTransferService.java
+++ src/main/java/org/fisco/bcos/web3j/precompile/dag2/DagTransferService.java
@@ -13,31 +13,6 @@ import org.fisco.bcos.web3j.tx.gas.ContractGasProvider;
 import org.fisco.bcos.web3j.tx.gas.StaticGasProvider;
 import org.fisco.bcos.web3j.precompile.dag2.DagTransfer;
 
-/*
- pragma solidity ^0.4.25;
-contract DagTransfer {
-    function userAdd(string user, uint256 balance) public returns(bool)
-    {
-        return true;   
-    }
-    function userSave(string user, uint256 balance) public returns(bool)
-    {
-        return true;
-    }
-    function userDraw(string user, uint256 balance) public returns(bool)
-    {
-        return true;
-    }
-    function userBalance(string user) public constant returns(bool,uint256)
-    {
-        return (true, 0);
-    }
-    function userTransfer(string user_a, string user_b, uint256 amount) public returns(bool)
-    {
-        return true;
-    }
-}
- */
 public class DagTransferService {
 
 	private static BigInteger gasPrice = new BigInteger("300000000");
