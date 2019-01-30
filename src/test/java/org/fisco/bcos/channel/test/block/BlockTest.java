939f0a5e1702725ad37b6a9d4fc4d520d1d6cbef
shuoyanhe <yanyanho@126.com>
2019-01-30 17:30:39 +0800

    add the node checkout sdk crt

--- src/test/java/org/fisco/bcos/channel/test/block/BlockTest.java
+++ src/test/java/org/fisco/bcos/channel/test/block/BlockTest.java
@@ -3,6 +3,7 @@ package org.fisco.bcos.channel.test.block;
 import org.fisco.bcos.channel.test.TestBase;
 import org.fisco.bcos.web3j.protocol.core.DefaultBlockParameter;
 import org.fisco.bcos.web3j.protocol.core.methods.response.EthBlock;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.math.BigInteger;
