bb24a2780a24f1d5dc4897031e6cf6931b1ab600
shuoyanhe <yanyanho@126.com>
2019-02-15 14:51:38 +0800

    revert the helloworld

--- src/test/resources/contract/HelloWorld.sol
+++ src/test/resources/contract/HelloWorld.sol
@@ -1,17 +1,17 @@
-pragma solidity ^0.5.2;
+pragma solidity ^0.4.2;
 
 contract HelloWorld{
-   string  name;
+    string name;
 
-     constructor() public {
+    function HelloWorld(){
        name = "Hello, World!";
     }
 
-    function get() public view  returns(string memory ){
+    function get()constant returns(string){
         return name;
     }
 
-    function set(string memory n) public {
+    function set(string n){
     	name = n;
     }
 }
