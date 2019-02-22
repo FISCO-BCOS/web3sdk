4a7cb39d53be1631135765837fa2b1732b8edc92
fqliao <fqliao@yeah.net>
2019-02-22 20:03:24 +0800

    fix console null excepiton and fix OkD sol

--- src/test/resources/contract/OkD.sol
+++ src/test/resources/contract/OkD.sol
@@ -1,4 +1,4 @@
-import "./DB.sol";
+import "./Table.sol";
 
 contract OkD{
     event insertResult(int count);
@@ -28,11 +28,11 @@ contract OkD{
         to.account=0x2;
         to.balance=0;
 
-        DBFactory df = DBFactory(0x1001);
-        df.createTable("t_ok", "from_accout", "from_balance,to_accout,to_balance");
-        df = DBFactory(0x1001);
-        DB db = df.openTable("t_ok");
-        Entry entry = db.newEntry();
+        TableFactory tf = TableFactory(0x1001);
+        tf.createTable("t_ok", "from_accout", "from_balance,to_accout,to_balance");
+        tf = TableFactory(0x1001);
+        Table table = tf.openTable("t_ok");
+        Entry entry = table.newEntry();
         entry.set("from_accout", "0x1");
         entry.set("from_balance", 10000000000);
         entry.set("to_accout", "0x2");
@@ -47,14 +47,14 @@ contract OkD{
     	from.balance = from.balance - num;
     	to.balance += num;
 
-        DBFactory df = DBFactory(0x1001);
-        DB db = df.openTable("t_ok");
-        Entry entry = db.newEntry();
+        TableFactory tf = TableFactory(0x1001);
+        Table table = tf.openTable("t_ok");
+        Entry entry = table.newEntry();
         entry.set("from_accout", from_accout);
         entry.set("from_balance", from.balance);
         entry.set("to_accout", "0x2");
         entry.set("to_balance", to.balance);
-        int count = db.insert(from_accout, entry);
+        int count = table.insert(from_accout, entry);
         insertResult(count);
         
     	// log.push(Translog("20170413",from.account,to.account,num));
