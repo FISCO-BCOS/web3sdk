bf50543209c012b60fc07e8aa0be1d920ff57c1a
shuoyanhe <yanyanho@126.com>
2019-02-25 11:57:50 +0800

    fix the sol2java loacation

--- solidity/sol2java.sh
+++ solidity/sol2java.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cd ..
 echo "Please ensure  gradle version is 4.10.1 and above!";
 gradle run -Dexec.mainClass=org.fisco.bcos.web3j.solidity.CompileSolToJava --args="$1"
+echo "Please ensure  gradle version is 4.10.1 and above!";
