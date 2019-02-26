8124e9694723edaeb388a79bff0e7ca40a48bb4f
fqliao <fqliao@yeah.net>
2019-02-26 17:08:39 +0800

    rename permission comands and remove abbr comand

--- src/test/java/org/fisco/bcos/web3j/console/ConsoleClient.java
+++ src/test/java/org/fisco/bcos/web3j/console/ConsoleClient.java
@@ -109,7 +109,7 @@ public class ConsoleClient {
         System.out.print("");
         continue;
       }
-      if ("quit".equals(params[0]) || "q".equals(params[0])) {
+      if ("quit".equals(params[0])) {
         if (HelpInfo.promptNoParams(params, "q")) {
           continue;
         } else if (params.length > 2) {
@@ -122,191 +122,144 @@ public class ConsoleClient {
       try {
         switch (params[0]) {
           case "help":
-          case "h":
             console.help(params);
             break;
           case "getBlockNumber":
-          case "gbn":
             console.getBlockNumber(params);
             break;
           case "getPbftView":
-          case "gpv":
             console.getPbftView(params);
             break;
           case "getSealerList":
-          case "gsl":
             console.getSealerList(params);
             break;
           case "getObserverList":
-          case "gol":
             console.getObserverList(params);
             break;
           case "getConsensusStatus":
-          case "gcs":
             console.getConsensusStatus(params);
             break;
           case "getSyncStatus":
-          case "gss":
             console.getSyncStatus(params);
             break;
           case "getClientVersion":
-          case "gcv":
             console.getClientVersion(params);
             break;
           case "getPeers":
-          case "gps":
             console.getPeers(params);
             break;
           case "getNodeIDList":
-          case "gnl":
             console.getNodeIDList(params);
             break;
           case "getGroupPeers":
-          case "ggp":
             console.getGroupPeers(params);
             break;
           case "getGroupList":
-          case "ggl":
             console.getGroupList(params);
             break;
           case "getBlockByHash":
-          case "gbbh":
             console.getBlockByHash(params);
             break;
           case "getBlockByNumber":
-          case "gbbn":
             console.getBlockByNumber(params);
             break;
           case "getBlockHashByNumber":
-          case "ghbn":
             console.getBlockHashByNumber(params);
             break;
           case "getTransactionByHash":
-          case "gtbh":
             console.getTransactionByHash(params);
             break;
           case "getTransactionByBlockHashAndIndex":
-          case "gthi":
             console.getTransactionByBlockHashAndIndex(params);
             break;
           case "getTransactionByBlockNumberAndIndex":
-          case "gtni":
             console.getTransactionByBlockNumberAndIndex(params);
             break;
           case "getTransactionReceipt":
-          case "gtr":
             console.getTransactionReceipt(params);
             break;
           case "getPendingTransactions":
-          case "gpt":
             console.getPendingTransactions(params);
             break;
           case "getPendingTxSize":
-          case "gpts":
             console.getPendingTxSize(params);
             break;
           case "getCode":
-          case "gc":
             console.getCode(params);
             break;
           case "getTotalTransactionCount":
-          case "gtc":
             console.getTotalTransactionCount(params);
             break;
           case "deploy":
-          case "d":
             console.deploy(params);
             break;
           case "call":
-          case "c":
             console.call(params);
             break;
           case "deployByCNS":
-          case "dbc":
             console.deployByCNS(params);
             break;
           case "callByCNS":
-          case "cbc":
             console.callByCNS(params);
             break;
           case "queryCNS":
-          case "qcs":
             console.queryCNS(params);
             break;
           case "addSealer":
-          case "as":
             console.addSealer(params);
             break;
           case "addObserver":
-          case "ao":
             console.addObserver(params);
             break;
           case "removeNode":
-          case "rn":
             console.removeNode(params);
             break;
           case "addUserTableManager":
-          case "aum":
             console.addUserTableManager(params);
             break;
           case "removeUserTableManager":
-          case "rum":
             console.removeUserTableManager(params);
             break;
           case "queryUserTableManager":
-          case "qum":
             console.queryUserTableManager(params);
             break;
           case "addDeployAndCreateManager":
-          case "adm":
             console.addDeployAndCreateManager(params);
             break;
           case "removeDeployAndCreateManager":
-          case "rdm":
             console.removeDeployAndCreateManager(params);
             break;
           case "queryDeployAndCreateManager":
-          case "qdm":
             console.queryDeployAndCreateManager(params);
             break;
           case "addAuthorityManager":
-          case "aam":
             console.addAuthorityManager(params);
             break;
           case "removeAuthorityManager":
-          case "ram":
             console.removeAuthorityManager(params);
             break;
           case "queryAuthorityManager":
-          case "qam":
             console.queryAuthorityManager(params);
             break;
           case "addNodeManager":
-          case "anm":
             console.addNodeManager(params);
             break;
           case "removeNodeManager":
-          case "rnm":
             console.removeNodeManager(params);
             break;
           case "queryNodeManager":
-          case "qnm":
             console.queryNodeManager(params);
             break;
           case "addCNSManager":
-          case "acm":
             console.addCNSManager(params);
             break;
           case "removeCNSManager":
-          case "rcm":
             console.removeCNSManager(params);
             break;
           case "queryCNSManager":
-          case "qcm":
             console.queryCNSManager(params);
             break;
           case "addSysConfigManager":
-          case "asm":
             console.addSysConfigManager(params);
             break;
           case "removeSysConfigManager":
@@ -314,15 +267,12 @@ public class ConsoleClient {
             console.removeSysConfigManager(params);
             break;
           case "querySysConfigManager":
-          case "qsm":
             console.querySysConfigManager(params);
             break;
           case "setSystemConfigByKey":
-          case "ssc":
             console.setSystemConfigByKey(params);
             break;
           case "getSystemConfigByKey":
-          case "gsc":
             console.getSystemConfigByKey(params);
             break;
           default:
