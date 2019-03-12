13635fb8fe603071e28e1a2a95781868baad9ba0
octopuswang <octopuswang@webank.com>
2019-03-12 17:20:09 +0800

    add log output.

--- src/test/java/org/fisco/bcos/channel/test/dag/DagTransferUser.java
+++ src/test/java/org/fisco/bcos/channel/test/dag/DagTransferUser.java
@@ -2,7 +2,11 @@ package org.fisco.bcos.channel.test.dag;
 
 import java.math.BigInteger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DagTransferUser {
+	private static Logger logger = LoggerFactory.getLogger(DagTransferUser.class);
 	private String user;
 	private BigInteger amount;
 	
@@ -24,10 +28,14 @@ public class DagTransferUser {
 	}
 	
 	synchronized public void increase(BigInteger amount) {
+		logger.debug("increase before amount is " + this.amount);
 		this.amount = this.amount.add(amount);
+		logger.debug("increase after amount is " + this.amount);
 	}
 	
 	synchronized public void decrease(BigInteger amount) {
+		logger.debug("decrease before amount is " + this.amount);
 		this.amount = this.amount.subtract(amount);
+		logger.debug("decrease after amount is " + this.amount);
 	}
 }
