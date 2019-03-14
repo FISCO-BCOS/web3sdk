21b7675ae3301db5106acdbefe5bd4b2fc965273
fqliao <fqliao@yeah.net>
2019-03-14 16:25:50 +0800

    integration test for web3j api

--- src/integration-test/java/org/fisco/bcos/TestBase.java
+++ src/integration-test/java/org/fisco/bcos/TestBase.java
@@ -1,9 +1,13 @@
 package org.fisco.bcos;
 
+import java.math.BigInteger;
+
 import org.fisco.bcos.channel.client.Service;
+import org.fisco.bcos.channel.test.guomi.Ok;
 import org.fisco.bcos.web3j.crypto.Credentials;
 import org.fisco.bcos.web3j.protocol.Web3j;
 import org.fisco.bcos.web3j.protocol.channel.ChannelEthereumService;
+import org.fisco.bcos.web3j.tx.gas.StaticGasProvider;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.springframework.context.ApplicationContext;
@@ -11,9 +15,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestBase {
   public static ApplicationContext context = null;
-  public Credentials credentials =
+  public static Credentials credentials =
       Credentials.create("b83261efa42895c38c6c2364ca878f43e77f3cddbc922bf57d0d48070f79feb6");
   protected static Web3j web3j;
+  protected static BigInteger gasPrice = new BigInteger("30000000");
+  protected static BigInteger gasLimit = new BigInteger("30000000");
+  protected static String address;
+  protected static BigInteger blockNumber;
+  protected static String blockHash;
+  protected static String txHash;
 
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
@@ -30,6 +40,12 @@ public class TestBase {
     channelEthereumService.setChannelService(service);
 
     web3j = Web3j.build(channelEthereumService, service.getGroupId());
+   
+    Ok ok = Ok.deploy(web3j, credentials, new StaticGasProvider(gasPrice, gasLimit)).send();
+    address = ok.getContractAddress();
+    blockNumber = ok.getTransactionReceipt().get().getBlockNumber();
+    blockHash = ok.getTransactionReceipt().get().getBlockHash();
+    txHash = ok.getTransactionReceipt().get().getTransactionHash();
   }
 
   @AfterClass
