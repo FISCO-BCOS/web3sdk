887b1ad5d624e70473dc0491d815d509f53e4d76
fqliao <fqliao@yeah.net>
2019-05-17 15:16:18 +0800

    format code

--- src/main/java/org/fisco/bcos/channel/client/KeyStoreManager.java
+++ src/main/java/org/fisco/bcos/channel/client/KeyStoreManager.java
@@ -10,7 +10,6 @@ import java.security.PublicKey;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-
 import org.fisco.bcos.web3j.crypto.ECKeyPair;
 import org.fisco.bcos.web3j.crypto.Keys;
 import org.springframework.core.io.Resource;
@@ -18,54 +17,58 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
 public class KeyStoreManager {
-	public void load() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
-		ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-		keyStore = KeyStore.getInstance("PKCS12");
-		Resource keyStoreResource = resolver.getResource(keyStoreFile);
+    public void load()
+            throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        keyStore = KeyStore.getInstance("PKCS12");
+        Resource keyStoreResource = resolver.getResource(keyStoreFile);
 
-		keyStore.load(keyStoreResource.getInputStream(), password.toCharArray());
-	}
-	
-	public void load(InputStream in, String password) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
-		keyStore.load(in, password.toCharArray());
-	}
-	
-	public PrivateKey getPrivateKey(String name, String password) throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
-		return (PrivateKey) keyStore.getKey(name, password.toCharArray());
-	}
-	
-	public Certificate getCertificate(String name) throws KeyStoreException {
-		return keyStore.getCertificate(name);
-	}
-	
-	public PublicKey getPublicKey(String name) throws KeyStoreException {
-		Certificate certificate = getCertificate(name);
-		return certificate.getPublicKey();
-	}
-	
-	public ECKeyPair getECKeyPair(String name, String password) throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
-		PrivateKey privateKey = getPrivateKey(name, password);
-		
-		return Keys.deserialize(privateKey.getEncoded());
-	}
-	
-	public String getPassword() {
-		return password;
-	}
+        keyStore.load(keyStoreResource.getInputStream(), password.toCharArray());
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void load(InputStream in, String password)
+            throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+        keyStore.load(in, password.toCharArray());
+    }
 
-	public String getKeyStoreFile() {
-		return keyStoreFile;
-	}
+    public PrivateKey getPrivateKey(String name, String password)
+            throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
+        return (PrivateKey) keyStore.getKey(name, password.toCharArray());
+    }
 
-	public void setKeyStoreFile(String keyStoreFile) {
-		this.keyStoreFile = keyStoreFile;
-	}
+    public Certificate getCertificate(String name) throws KeyStoreException {
+        return keyStore.getCertificate(name);
+    }
 
-	private String keyStoreFile;
-	private String password;
-	private KeyStore keyStore;
+    public PublicKey getPublicKey(String name) throws KeyStoreException {
+        Certificate certificate = getCertificate(name);
+        return certificate.getPublicKey();
+    }
+
+    public ECKeyPair getECKeyPair(String name, String password)
+            throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
+        PrivateKey privateKey = getPrivateKey(name, password);
+
+        return Keys.deserialize(privateKey.getEncoded());
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getKeyStoreFile() {
+        return keyStoreFile;
+    }
+
+    public void setKeyStoreFile(String keyStoreFile) {
+        this.keyStoreFile = keyStoreFile;
+    }
+
+    private String keyStoreFile;
+    private String password;
+    private KeyStore keyStore;
 }
