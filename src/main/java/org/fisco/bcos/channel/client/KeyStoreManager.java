46de8c257ea035c7605d55e9bb00da7ce95664a8
fqliao <fqliao@yeah.net>
2019-05-19 22:09:38 +0800

    add name property for KeyStoreManager

--- src/main/java/org/fisco/bcos/channel/client/KeyStoreManager.java
+++ src/main/java/org/fisco/bcos/channel/client/KeyStoreManager.java
@@ -20,7 +20,6 @@ import java.security.spec.ECPoint;
 import java.security.spec.ECPublicKeySpec;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Collections;
-
 import org.bouncycastle.jcajce.provider.keystore.pkcs12.PKCS12KeyStoreSpi;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.fisco.bcos.web3j.crypto.ECKeyPair;
@@ -29,102 +28,125 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
 public class KeyStoreManager {
-	private String keyStoreFile;
-	private String password;
-	private KeyStore keyStore;
-	PKCS12KeyStoreSpi p12KeyStore;
-	
-	KeyStoreManager() {
-		Security.setProperty("crypto.policy", "unlimited");
-		Security.addProvider(new BouncyCastleProvider());
-	}
+    private String keyStoreFile;
+    private String name;
+    private String password;
+    private KeyStore keyStore;
+    PKCS12KeyStoreSpi p12KeyStore;
 
-	public void load() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException,
-			NoSuchProviderException {
-		ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-		keyStore = KeyStore.getInstance("PKCS12", "BC");
-		Resource keyStoreResource = resolver.getResource(keyStoreFile);
+    KeyStoreManager() {
+        Security.setProperty("crypto.policy", "unlimited");
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
-		keyStore.load(keyStoreResource.getInputStream(), password.toCharArray());
-	}
+    public void load()
+            throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException,
+                    NoSuchProviderException {
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        keyStore = KeyStore.getInstance("PKCS12", "BC");
+        Resource keyStoreResource = resolver.getResource(keyStoreFile);
 
-	public void load(InputStream in, String password)
-			throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
-		// keyStore.load(in, password.toCharArray());
+        keyStore.load(keyStoreResource.getInputStream(), password.toCharArray());
+    }
 
-		p12KeyStore.engineLoad(in, password.toCharArray());
-	}
+    public void load(InputStream in, String password)
+            throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+        // keyStore.load(in, password.toCharArray());
 
-	public PrivateKey getPrivateKey(String name, String password)
-			throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
-		return (PrivateKey) keyStore.getKey(name, password.toCharArray());
-	}
+        p12KeyStore.engineLoad(in, password.toCharArray());
+    }
 
-	public PublicKey getPublicKey(String name, String password) throws UnrecoverableKeyException, KeyStoreException,
-			NoSuchAlgorithmException, InvalidKeySpecException, NoSuchProviderException {
-		ECPrivateKey privateKey = (ECPrivateKey) getPrivateKey(name, password);
+    public PrivateKey getPrivateKey(String name, String password)
+            throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
+        return (PrivateKey) keyStore.getKey(name, password.toCharArray());
+    }
 
-		ECParameterSpec params = privateKey.getParams();
+    public PublicKey getPublicKey(String name, String password)
+            throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException,
+                    InvalidKeySpecException, NoSuchProviderException {
+        ECPrivateKey privateKey = (ECPrivateKey) getPrivateKey(name, password);
 
-		org.bouncycastle.jce.spec.ECParameterSpec bcSpec = org.bouncycastle.jcajce.provider.asymmetric.util.EC5Util
-				.convertSpec(params, false);
-		org.bouncycastle.math.ec.ECPoint q = bcSpec.getG().multiply(privateKey.getS());
-		org.bouncycastle.math.ec.ECPoint bcW = bcSpec.getCurve().decodePoint(q.getEncoded(false));
-		ECPoint w = new ECPoint(bcW.getAffineXCoord().toBigInteger(), bcW.getAffineYCoord().toBigInteger());
-		ECPublicKeySpec keySpec = new ECPublicKeySpec(w, tryFindNamedCurveSpec(params));
-		return (PublicKey) KeyFactory
-				.getInstance("EC", org.bouncycastle.jce.provider.BouncyCastleProvider.PROVIDER_NAME)
-				.generatePublic(keySpec);
-	}
+        ECParameterSpec params = privateKey.getParams();
 
-	@SuppressWarnings("unchecked")
-	public static ECParameterSpec tryFindNamedCurveSpec(ECParameterSpec params) {
-		org.bouncycastle.jce.spec.ECParameterSpec bcSpec = org.bouncycastle.jcajce.provider.asymmetric.util.EC5Util
-				.convertSpec(params, false);
-		for (Object name : Collections.list(org.bouncycastle.jce.ECNamedCurveTable.getNames())) {
-			org.bouncycastle.jce.spec.ECNamedCurveParameterSpec bcNamedSpec = org.bouncycastle.jce.ECNamedCurveTable
-					.getParameterSpec((String) name);
-			if (bcNamedSpec.getN().equals(bcSpec.getN()) && bcNamedSpec.getH().equals(bcSpec.getH())
-					&& bcNamedSpec.getCurve().equals(bcSpec.getCurve()) && bcNamedSpec.getG().equals(bcSpec.getG())) {
-				return new org.bouncycastle.jce.spec.ECNamedCurveSpec(bcNamedSpec.getName(), bcNamedSpec.getCurve(),
-						bcNamedSpec.getG(), bcNamedSpec.getN(), bcNamedSpec.getH(), bcNamedSpec.getSeed());
-			}
-		}
-		return params;
-	}
+        org.bouncycastle.jce.spec.ECParameterSpec bcSpec =
+                org.bouncycastle.jcajce.provider.asymmetric.util.EC5Util.convertSpec(params, false);
+        org.bouncycastle.math.ec.ECPoint q = bcSpec.getG().multiply(privateKey.getS());
+        org.bouncycastle.math.ec.ECPoint bcW = bcSpec.getCurve().decodePoint(q.getEncoded(false));
+        ECPoint w =
+                new ECPoint(
+                        bcW.getAffineXCoord().toBigInteger(), bcW.getAffineYCoord().toBigInteger());
+        ECPublicKeySpec keySpec = new ECPublicKeySpec(w, tryFindNamedCurveSpec(params));
+        return (PublicKey)
+                KeyFactory.getInstance(
+                                "EC",
+                                org.bouncycastle.jce.provider.BouncyCastleProvider.PROVIDER_NAME)
+                        .generatePublic(keySpec);
+    }
 
-	public Certificate getCertificate(String name) throws KeyStoreException {
-		return keyStore.getCertificate(name);
-	}
+    @SuppressWarnings("unchecked")
+    public static ECParameterSpec tryFindNamedCurveSpec(ECParameterSpec params) {
+        org.bouncycastle.jce.spec.ECParameterSpec bcSpec =
+                org.bouncycastle.jcajce.provider.asymmetric.util.EC5Util.convertSpec(params, false);
+        for (Object name : Collections.list(org.bouncycastle.jce.ECNamedCurveTable.getNames())) {
+            org.bouncycastle.jce.spec.ECNamedCurveParameterSpec bcNamedSpec =
+                    org.bouncycastle.jce.ECNamedCurveTable.getParameterSpec((String) name);
+            if (bcNamedSpec.getN().equals(bcSpec.getN())
+                    && bcNamedSpec.getH().equals(bcSpec.getH())
+                    && bcNamedSpec.getCurve().equals(bcSpec.getCurve())
+                    && bcNamedSpec.getG().equals(bcSpec.getG())) {
+                return new org.bouncycastle.jce.spec.ECNamedCurveSpec(
+                        bcNamedSpec.getName(),
+                        bcNamedSpec.getCurve(),
+                        bcNamedSpec.getG(),
+                        bcNamedSpec.getN(),
+                        bcNamedSpec.getH(),
+                        bcNamedSpec.getSeed());
+            }
+        }
+        return params;
+    }
 
-	public PublicKey getPublicKeyFromCertificate(String name) throws KeyStoreException {
-		Certificate certificate = getCertificate(name);
-		return certificate.getPublicKey();
-	}
+    public Certificate getCertificate(String name) throws KeyStoreException {
+        return keyStore.getCertificate(name);
+    }
 
-	public ECKeyPair getECKeyPair(String name, String password) throws UnrecoverableKeyException, KeyStoreException,
-			NoSuchAlgorithmException, InvalidKeySpecException, NoSuchProviderException {
-		PrivateKey privateKey = getPrivateKey(name, password);
-		PublicKey publicKey = getPublicKey(name, password);
+    public PublicKey getPublicKeyFromCertificate(String name) throws KeyStoreException {
+        Certificate certificate = getCertificate(name);
+        return certificate.getPublicKey();
+    }
 
-		KeyPair keyPair = new KeyPair(publicKey, privateKey);
+    public ECKeyPair getECKeyPair(String name, String password)
+            throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException,
+                    InvalidKeySpecException, NoSuchProviderException {
+        PrivateKey privateKey = getPrivateKey(name, password);
+        PublicKey publicKey = getPublicKey(name, password);
 
-		return ECKeyPair.create(keyPair);
-	}
+        KeyPair keyPair = new KeyPair(publicKey, privateKey);
 
-	public String getPassword() {
-		return password;
-	}
+        return ECKeyPair.create(keyPair);
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public String getKeyStoreFile() {
-		return keyStoreFile;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public void setKeyStoreFile(String keyStoreFile) {
-		this.keyStoreFile = keyStoreFile;
-	}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKeyStoreFile() {
+        return keyStoreFile;
+    }
+
+    public void setKeyStoreFile(String keyStoreFile) {
+        this.keyStoreFile = keyStoreFile;
+    }
 }
