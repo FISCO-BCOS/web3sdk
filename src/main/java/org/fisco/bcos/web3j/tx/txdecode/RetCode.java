557b3bfc67e210518251ee235be0896d2fc0f34f
octopus <912554887@qq.com>
2019-06-12 23:56:12 +0800

    update decode input

--- src/main/java/org/fisco/bcos/web3j/tx/txdecode/RetCode.java
+++ src/main/java/org/fisco/bcos/web3j/tx/txdecode/RetCode.java
@@ -14,9 +14,6 @@
 
 package org.fisco.bcos.web3j.tx.txdecode;
 
-import lombok.Data;
-
-@Data
 public class RetCode {
     private Integer code;
     private String msg;
@@ -34,6 +31,22 @@ public class RetCode {
 
     public static RetCode mark(Integer code) {
         return new RetCode(code, null);
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
     }
 
     @Override
