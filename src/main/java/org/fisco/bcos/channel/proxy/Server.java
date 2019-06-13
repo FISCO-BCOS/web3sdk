6b86819c76e6237b5ed40733dd2f345feb30e43c
ancelmo <ancelmo@webank.com>
2019-06-13 18:44:57 +0800

    support multicast

--- src/main/java/org/fisco/bcos/channel/proxy/Server.java
+++ src/main/java/org/fisco/bcos/channel/proxy/Server.java
@@ -1,5 +1,6 @@
 package org.fisco.bcos.channel.proxy;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
