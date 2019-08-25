aaa21111ac7168b58f61d390c50bfbd575bc152d
bxq2011hust <bxq2011hust@qq.com>
2019-08-25 18:56:07 +0800

    rename node.crt to sdk.crt

--- src/main/java/org/fisco/bcos/channel/handler/GroupChannelConnectionsConfig.java
+++ src/main/java/org/fisco/bcos/channel/handler/GroupChannelConnectionsConfig.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class GroupChannelConnectionsConfig {
     private List<ChannelConnections> allChannelConnections;
+    private String caCert = "classpath:ca.crt";
+    private String sslCert = "classpath:node.crt";
+    private String sslKey = "classpath:node.key";
 
     public List<ChannelConnections> getAllChannelConnections() {
         return allChannelConnections;
@@ -11,5 +14,29 @@ public class GroupChannelConnectionsConfig {
 
     public void setAllChannelConnections(List<ChannelConnections> allChannelConnections) {
         this.allChannelConnections = allChannelConnections;
+    }
+
+    public void setSslKey(String sslKey) {
+        this.sslKey = sslKey;
+    }
+
+    public void setSslCert(String sslCert) {
+        this.sslCert = sslCert;
+    }
+
+    public void setCaCert(String caCert) {
+        this.caCert = caCert;
+    }
+
+    public String getCaCert() {
+        return caCert;
+    }
+
+    public String getSslCert() {
+        return sslCert;
+    }
+
+    public String getSslKey() {
+        return sslKey;
     }
 }
