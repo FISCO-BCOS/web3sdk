c9634bb5658fc99cf34a919ac0e9fbe92e69a60c
MO NAN <651932351@qq.com>
2019-08-27 11:16:06 +0800

    replace ca string config to resource (#387)
    * change cert from file to resource
    
    * add default value & comment
    
    * reduce Service run()
    
    * update format
    
    * manual trigger build
    
    * fix format
    
    * move static var to front
    
    * fix warnings
    
    * fix sider
    
    * update var name

--- src/main/java/org/fisco/bcos/channel/handler/GroupChannelConnectionsConfig.java
+++ src/main/java/org/fisco/bcos/channel/handler/GroupChannelConnectionsConfig.java
@@ -1,12 +1,14 @@
 package org.fisco.bcos.channel.handler;
 
 import java.util.List;
+import org.springframework.core.io.Resource;
 
 public class GroupChannelConnectionsConfig {
     private List<ChannelConnections> allChannelConnections;
-    private String caCert = "classpath:ca.crt";
-    private String sslCert = "classpath:node.crt";
-    private String sslKey = "classpath:node.key";
+
+    private Resource caCert;
+    private Resource sslCert;
+    private Resource sslKey;
 
     public List<ChannelConnections> getAllChannelConnections() {
         return allChannelConnections;
@@ -16,27 +18,27 @@ public class GroupChannelConnectionsConfig {
         this.allChannelConnections = allChannelConnections;
     }
 
-    public void setSslKey(String sslKey) {
-        this.sslKey = sslKey;
-    }
-
-    public void setSslCert(String sslCert) {
-        this.sslCert = sslCert;
-    }
-
-    public void setCaCert(String caCert) {
-        this.caCert = caCert;
-    }
-
-    public String getCaCert() {
+    public Resource getCaCert() {
         return caCert;
     }
 
-    public String getSslCert() {
+    public void setCaCert(Resource caCert) {
+        this.caCert = caCert;
+    }
+
+    public Resource getSslCert() {
         return sslCert;
     }
 
-    public String getSslKey() {
+    public void setSslCert(Resource sslCert) {
+        this.sslCert = sslCert;
+    }
+
+    public Resource getSslKey() {
         return sslKey;
+    }
+
+    public void setSslKey(Resource sslKey) {
+        this.sslKey = sslKey;
     }
 }
