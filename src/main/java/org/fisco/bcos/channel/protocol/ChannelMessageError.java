62a3c1628e08fbb9d0f8571a14bb61f406810e37
MO NAN <651932351@qq.com>
2019-08-30 16:02:53 +0800

    add receipt error callback (#392)
    add error code for decode error

--- src/main/java/org/fisco/bcos/channel/protocol/ChannelMessageError.java
+++ src/main/java/org/fisco/bcos/channel/protocol/ChannelMessageError.java
@@ -3,7 +3,8 @@ package org.fisco.bcos.channel.protocol;
 public enum ChannelMessageError {
     NODES_UNREACHABLE(99), // nodes unreachable
     MESSAGE_SEND_EXCEPTION(100), // send failed after N times retry
-    MESSAGE_TIMEOUT(102); // timeout
+    MESSAGE_TIMEOUT(102), // timeout
+    MESSAGE_DECODE_ERROR(105); // decode error
 
     private int error;
 
