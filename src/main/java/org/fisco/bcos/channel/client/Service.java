bcb76c649b0a5eb833daf5e82239747485d3fa1c
octopus <912554887@qq.com>
2019-09-05 19:28:02 +0800

    fix upload tool compiled error

--- src/main/java/org/fisco/bcos/channel/client/Service.java
+++ src/main/java/org/fisco/bcos/channel/client/Service.java
@@ -813,7 +813,6 @@ public class Service {
     /**
      * @param params
      * @param callback
-     * @param timeout
      */
     public void registerEventLogFilter(EventLogUserParams params, EventLogPushCallback callback) {
 
@@ -838,11 +837,7 @@ public class Service {
                 params);
     }
 
-    /**
-     * @param filter
-     * @param callback
-     * @throws JsonProcessingException
-     */
+    /** @param filter */
     public void asyncSendRegisterEventLogFilterMessage(EventLogFilter filter) {
 
         ChannelRequest request = new ChannelRequest();
