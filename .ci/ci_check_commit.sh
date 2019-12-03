8de50852303d30ee83dde885a4ddb30e3265ed48
octopus <912554887@qq.com>
2019-12-03 20:29:24 +0800

    update ci_check_commit.sh

--- .ci/ci_check_commit.sh
+++ .ci/ci_check_commit.sh
@@ -3,7 +3,7 @@
 set -e
 
 scan_code_script="cobra/cobra.py -f json -o /tmp/report.json -t "
-ignore_files=(PerformanceOkDSync.java SM2Algorithm.java SM2KeyGenerator.java test)
+ignore_files=(Constant.java PerformanceOkDSync.java SM2Algorithm.java SM2KeyGenerator.java test)
 
 LOG_ERROR() {
     content=${1}
