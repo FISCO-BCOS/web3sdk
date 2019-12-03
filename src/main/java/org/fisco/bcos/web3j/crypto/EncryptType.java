4263567ff282014ddb887e355cfe93cc856c1c06
ywy2090 <912554887@qq.com>
2019-12-03 20:39:43 +0800

    Release 2.2.0 fisco (#464)
    * check int/uint range before abi encode.
    
    * add gm support for parallel test.
    
    * update ci_check_commit.sh

--- src/main/java/org/fisco/bcos/web3j/crypto/EncryptType.java
+++ src/main/java/org/fisco/bcos/web3j/crypto/EncryptType.java
@@ -3,15 +3,18 @@ package org.fisco.bcos.web3j.crypto;
 import org.fisco.bcos.web3j.crypto.gm.sm2.SM2Sign;
 import org.fisco.bcos.web3j.crypto.gm.sm3.SM3Digest;
 
-/** Created by websterchen on 2018/4/25. */
 public class EncryptType {
-    public static int encryptType = 0; // 0:ECDSA 1:SM2
+
+    public static final int ECDSA_TYPE = 0;
+    public static final int SM2_TYPE = 1;
+
+    public static int encryptType = ECDSA_TYPE; // 0:ECDSA 1:SM2
 
     public EncryptType(int encryptType) {
         EncryptType.encryptType = encryptType;
         SignInterface signInterface;
         HashInterface hashInterface;
-        if (encryptType == 1) {
+        if (encryptType == SM2_TYPE) {
             signInterface = new SM2Sign();
             hashInterface = new SM3Digest();
         } else {
