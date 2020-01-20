5597d28d8cc609ec18c9ad90e64df5ae6ba9b3e3
ywy2090 <912554887@qq.com>
2020-01-20 17:30:35 +0800

    add frozen precompiled service (#495)
    * add contract frozen function
    
    * goJF format

---
+++ src/main/java/org/fisco/bcos/web3j/precompile/frozen/Frozen.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.2;
+
+contract Frozen {
+    function kill(address addr) public returns(int);
+    function freeze(address addr) public returns(int);
+    function unfreeze(address addr) public returns(int);
+    function queryStatus(address addr) public constant returns(uint,string);
+}
