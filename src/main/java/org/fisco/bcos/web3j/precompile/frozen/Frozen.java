4cdaebba03abdef0345884ea1d43f98db8311a49
ywy2090 <912554887@qq.com>
2020-02-19 12:49:50 +0800

    add queryAuthority to Frozen (#507)
    * update web3sdk version in release_node.txt
    
    * add queryAuthority method to Frozen contract.
    
    * add queryAuthority to Frozen

--- src/main/java/org/fisco/bcos/web3j/precompile/frozen/Frozen.java
+++ src/main/java/org/fisco/bcos/web3j/precompile/frozen/Frozen.java
@@ -9,12 +9,14 @@ import org.fisco.bcos.channel.client.TransactionSucCallback;
 import org.fisco.bcos.web3j.abi.FunctionReturnDecoder;
 import org.fisco.bcos.web3j.abi.TypeReference;
 import org.fisco.bcos.web3j.abi.datatypes.Address;
+import org.fisco.bcos.web3j.abi.datatypes.DynamicArray;
 import org.fisco.bcos.web3j.abi.datatypes.Function;
 import org.fisco.bcos.web3j.abi.datatypes.Type;
 import org.fisco.bcos.web3j.abi.datatypes.Utf8String;
 import org.fisco.bcos.web3j.abi.datatypes.generated.Int256;
 import org.fisco.bcos.web3j.abi.datatypes.generated.Uint256;
 import org.fisco.bcos.web3j.crypto.Credentials;
+import org.fisco.bcos.web3j.crypto.EncryptType;
 import org.fisco.bcos.web3j.protocol.Web3j;
 import org.fisco.bcos.web3j.protocol.core.RemoteCall;
 import org.fisco.bcos.web3j.protocol.core.methods.response.TransactionReceipt;
@@ -38,18 +40,24 @@ import org.fisco.bcos.web3j.tx.txdecode.TransactionDecoder;
  */
 @SuppressWarnings("unchecked")
 public class Frozen extends Contract {
-    public static String BINARY = "";
+    public static String BINARY =
+            "608060405234801561001057600080fd5b5061032c806100206000396000f30060806040526004361061006d576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806345c8b1a6146100725780638d1fdf2f146100c9578063afe7b30214610120578063b9d4f0cd146101e3578063cbf0b0c014610282575b600080fd5b34801561007e57600080fd5b506100b3600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506102d9565b6040518082815260200191505060405180910390f35b3480156100d557600080fd5b5061010a600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506102e0565b6040518082815260200191505060405180910390f35b34801561012c57600080fd5b50610161600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506102e7565b6040518083815260200180602001828103825283818151815260200191508051906020019080838360005b838110156101a757808201518184015260208101905061018c565b50505050905090810190601f1680156101d45780820380516001836020036101000a031916815260200191505b50935050505060405180910390f35b3480156101ef57600080fd5b50610224600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506102f0565b6040518083815260200180602001828103825283818151815260200191508051906020019060200280838360005b8381101561026d578082015181840152602081019050610252565b50505050905001935050505060405180910390f35b34801561028e57600080fd5b506102c3600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506102f9565b6040518082815260200191505060405180910390f35b6000919050565b6000919050565b60006060915091565b60006060915091565b60009190505600a165627a7a72305820d99d69ea8b3a59034686514b5be54bdece07894b542835cf23b85e685aebddf50029";
 
     public static final String ABI =
-            "[{\"constant\":false,\"inputs\":[{\"name\":\"addr\",\"type\":\"address\"}],\"name\":\"unfreeze\",\"outputs\":[{\"name\":\"\",\"type\":\"int256\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"addr\",\"type\":\"address\"}],\"name\":\"freeze\",\"outputs\":[{\"name\":\"\",\"type\":\"int256\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"addr\",\"type\":\"address\"}],\"name\":\"queryStatus\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"},{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"addr\",\"type\":\"address\"}],\"name\":\"kill\",\"outputs\":[{\"name\":\"\",\"type\":\"int256\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]";
+            "[{\"constant\":false,\"inputs\":[{\"name\":\"addr\",\"type\":\"address\"}],\"name\":\"unfreeze\",\"outputs\":[{\"name\":\"\",\"type\":\"int256\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"addr\",\"type\":\"address\"}],\"name\":\"freeze\",\"outputs\":[{\"name\":\"\",\"type\":\"int256\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"addr\",\"type\":\"address\"}],\"name\":\"queryStatus\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"},{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"addr\",\"type\":\"address\"}],\"name\":\"queryAuthority\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"},{\"name\":\"\",\"type\":\"address[]\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"addr\",\"type\":\"address\"}],\"name\":\"kill\",\"outputs\":[{\"name\":\"\",\"type\":\"int256\"}],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]";
 
     public static final TransactionDecoder transactionDecoder = new TransactionDecoder(ABI, BINARY);
+
+    public static String SM_BINARY =
+            "608060405234801561001057600080fd5b5061032c806100206000396000f30060806040526004361061006d576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806361cb24c3146100725780639c39c3d2146100c9578063d22c011f1461018c578063f12c66df146101e3578063fd7d15641461023a575b600080fd5b34801561007e57600080fd5b506100b3600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506102d9565b6040518082815260200191505060405180910390f35b3480156100d557600080fd5b5061010a600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506102e0565b6040518083815260200180602001828103825283818151815260200191508051906020019080838360005b83811015610150578082015181840152602081019050610135565b50505050905090810190601f16801561017d5780820380516001836020036101000a031916815260200191505b50935050505060405180910390f35b34801561019857600080fd5b506101cd600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506102e9565b6040518082815260200191505060405180910390f35b3480156101ef57600080fd5b50610224600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506102f0565b6040518082815260200191505060405180910390f35b34801561024657600080fd5b5061027b600480360381019080803573ffffffffffffffffffffffffffffffffffffffff1690602001909291905050506102f7565b6040518083815260200180602001828103825283818151815260200191508051906020019060200280838360005b838110156102c45780820151818401526020810190506102a9565b50505050905001935050505060405180910390f35b6000919050565b60006060915091565b6000919050565b6000919050565b600060609150915600a165627a7a72305820fa141f2e9387b79ebb276359827a9207a4b368db37c5e60431088ec29ea09dd70029";
 
     public static final String FUNC_UNFREEZE = "unfreeze";
 
     public static final String FUNC_FREEZE = "freeze";
 
     public static final String FUNC_QUERYSTATUS = "queryStatus";
+
+    public static final String FUNC_QUERYAUTHORITY = "queryAuthority";
 
     public static final String FUNC_KILL = "kill";
 
@@ -60,7 +68,7 @@ public class Frozen extends Contract {
             Credentials credentials,
             BigInteger gasPrice,
             BigInteger gasLimit) {
-        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+        super(getBinary(), contractAddress, web3j, credentials, gasPrice, gasLimit);
     }
 
     protected Frozen(
@@ -68,7 +76,7 @@ public class Frozen extends Contract {
             Web3j web3j,
             Credentials credentials,
             ContractGasProvider contractGasProvider) {
-        super(BINARY, contractAddress, web3j, credentials, contractGasProvider);
+        super(getBinary(), contractAddress, web3j, credentials, contractGasProvider);
     }
 
     @Deprecated
@@ -78,7 +86,7 @@ public class Frozen extends Contract {
             TransactionManager transactionManager,
             BigInteger gasPrice,
             BigInteger gasLimit) {
-        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+        super(getBinary(), contractAddress, web3j, transactionManager, gasPrice, gasLimit);
     }
 
     protected Frozen(
@@ -86,7 +94,11 @@ public class Frozen extends Contract {
             Web3j web3j,
             TransactionManager transactionManager,
             ContractGasProvider contractGasProvider) {
-        super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
+        super(getBinary(), contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public static String getBinary() {
+        return (EncryptType.encryptType == EncryptType.ECDSA_TYPE ? BINARY : SM_BINARY);
     }
 
     public static TransactionDecoder getTransactionDecoder() {
@@ -215,6 +227,26 @@ public class Frozen extends Contract {
                 });
     }
 
+    public RemoteCall<Tuple2<BigInteger, List<String>>> queryAuthority(String addr) {
+        final Function function =
+                new Function(
+                        FUNC_QUERYAUTHORITY,
+                        Arrays.<Type>asList(new org.fisco.bcos.web3j.abi.datatypes.Address(addr)),
+                        Arrays.<TypeReference<?>>asList(
+                                new TypeReference<Uint256>() {},
+                                new TypeReference<DynamicArray<Address>>() {}));
+        return new RemoteCall<Tuple2<BigInteger, List<String>>>(
+                new Callable<Tuple2<BigInteger, List<String>>>() {
+                    @Override
+                    public Tuple2<BigInteger, List<String>> call() throws Exception {
+                        List<Type> results = executeCallMultipleValueReturn(function);
+                        return new Tuple2<BigInteger, List<String>>(
+                                (BigInteger) results.get(0).getValue(),
+                                convertToNative((List<Address>) results.get(1).getValue()));
+                    }
+                });
+    }
+
     public RemoteCall<TransactionReceipt> kill(String addr) {
         final Function function =
                 new Function(
@@ -304,13 +336,15 @@ public class Frozen extends Contract {
 
     public static RemoteCall<Frozen> deploy(
             Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
-        return deployRemoteCall(Frozen.class, web3j, credentials, contractGasProvider, BINARY, "");
+        return deployRemoteCall(
+                Frozen.class, web3j, credentials, contractGasProvider, getBinary(), "");
     }
 
     @Deprecated
     public static RemoteCall<Frozen> deploy(
             Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
-        return deployRemoteCall(Frozen.class, web3j, credentials, gasPrice, gasLimit, BINARY, "");
+        return deployRemoteCall(
+                Frozen.class, web3j, credentials, gasPrice, gasLimit, getBinary(), "");
     }
 
     public static RemoteCall<Frozen> deploy(
@@ -318,7 +352,7 @@ public class Frozen extends Contract {
             TransactionManager transactionManager,
             ContractGasProvider contractGasProvider) {
         return deployRemoteCall(
-                Frozen.class, web3j, transactionManager, contractGasProvider, BINARY, "");
+                Frozen.class, web3j, transactionManager, contractGasProvider, getBinary(), "");
     }
 
     @Deprecated
@@ -328,6 +362,6 @@ public class Frozen extends Contract {
             BigInteger gasPrice,
             BigInteger gasLimit) {
         return deployRemoteCall(
-                Frozen.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
+                Frozen.class, web3j, transactionManager, gasPrice, gasLimit, getBinary(), "");
     }
 }
