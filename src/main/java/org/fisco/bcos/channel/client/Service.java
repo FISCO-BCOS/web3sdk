89ecb2120451c9cc1f00781a11d17bdad887d562
octopus <912554887@qq.com>
2020-02-21 16:00:34 +0800

    add topics update

--- src/main/java/org/fisco/bcos/channel/client/Service.java
+++ src/main/java/org/fisco/bcos/channel/client/Service.java
@@ -716,6 +716,52 @@ public class Service {
         }
     }
 
+    /**
+     * When SDK start, the initial subscribed topics information set by user will be sent to the
+     * node. User can update subscribed topics again by following steps: 1. Set the topics you want
+     * to subscribe to again Service service // Servcie object Set<String> topics // topics that
+     * subscribe again service.setTopics(topics) 2. send update topics message to all nodes
+     * service.updateTopicsToNode();
+     */
+    public void updateTopicsToNode() {
+
+        logger.info(" updateTopicToNode, groupId: {}, topics: {}", groupId, getTopics());
+
+        // select send node
+        ChannelConnections channelConnections =
+                allChannelConnections
+                        .getAllChannelConnections()
+                        .stream()
+                        .filter(x -> x.getGroupId() == groupId)
+                        .findFirst()
+                        .get();
+
+        if (Objects.isNull(channelConnections)) {
+            throw new IllegalArgumentException(
+                    " No group configuration was found, groupId: " + groupId);
+        }
+
+        ConnectionCallback callback = (ConnectionCallback) channelConnections.getCallback();
+        if (Objects.isNull(callback)) {
+            throw new IllegalArgumentException(
+                    " No callback was found for ChannelConnections, service is not initialized");
+        }
+        callback.setTopics(getTopics());
+
+        /** send update topic message to all connected nodes */
+        Map<String, ChannelHandlerContext> networkConnections =
+                channelConnections.getNetworkConnections();
+        for (ChannelHandlerContext ctx : networkConnections.values()) {
+            if (Objects.nonNull(ctx) && ChannelHandlerContextHelper.isChannelAvailable(ctx)) {
+                try {
+                    callback.sendUpdateTopicMessage(ctx);
+                } catch (Exception e) {
+                    logger.debug(" e: ", e);
+                }
+            }
+        }
+    }
+
     public void asyncMulticastChannelMessageForVerifyTopic(ChannelRequest request) {
         String toTopic = request.getToTopic();
         request.setToTopic(getNeedVerifyTopics(toTopic));
