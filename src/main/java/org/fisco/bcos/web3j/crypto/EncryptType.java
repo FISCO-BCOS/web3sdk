99d14052aee389d94cbbe7ec84664b67ecdf9d06
octopus <912554887@qq.com>
2020-03-08 16:03:17 +0800

    add private key type check when

--- src/main/java/org/fisco/bcos/web3j/crypto/EncryptType.java
+++ src/main/java/org/fisco/bcos/web3j/crypto/EncryptType.java
@@ -1,9 +1,15 @@
 package org.fisco.bcos.web3j.crypto;
 
+import java.security.spec.ECParameterSpec;
+import org.bouncycastle.jce.spec.ECNamedCurveSpec;
 import org.fisco.bcos.web3j.crypto.gm.sm2.SM2Sign;
 import org.fisco.bcos.web3j.crypto.gm.sm3.SM3Digest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EncryptType {
+
+    private static final Logger logger = LoggerFactory.getLogger(EncryptType.class);
 
     public static final int ECDSA_TYPE = 0;
     public static final int SM2_TYPE = 1;
@@ -11,6 +17,10 @@ public class EncryptType {
     public static int encryptType = ECDSA_TYPE; // 0:ECDSA 1:SM2
 
     public EncryptType(int encryptType) {
+        resetEncryptType(encryptType);
+    }
+
+    public void resetEncryptType(int encryptType) {
         EncryptType.encryptType = encryptType;
         SignInterface signInterface;
         HashInterface hashInterface;
@@ -25,11 +35,33 @@ public class EncryptType {
         Hash.setHashInterface(hashInterface);
     }
 
+    public static void checkAccountPrivateKey(ECParameterSpec ecParameterSpec, int encryptType) {
+        try {
+            String name = ((ECNamedCurveSpec) ecParameterSpec).getName();
+            boolean privateKeyMatch =
+                    ((EncryptType.ECDSA_TYPE != EncryptType.encryptType) && name.contains("sm2"))
+                            || ((EncryptType.ECDSA_TYPE == EncryptType.encryptType)
+                                    && name.contains("secp256k1"));
+
+            // logger.trace(" name: {}, encrypt: {}", name, encryptType);
+
+            if (!privateKeyMatch) {
+                throw new IllegalArgumentException(
+                        " Not matched private key type, type name: "
+                                + name
+                                + ", encryptType: "
+                                + EncryptType.encryptType);
+            }
+        } catch (Exception e) {
+            logger.error(" exception: {}", e);
+        }
+    }
+
     public int getEncryptType() {
         return encryptType;
     }
 
     public void setEncryptType(int encryptType) {
-        EncryptType.encryptType = encryptType;
+        resetEncryptType(encryptType);
     }
 }
