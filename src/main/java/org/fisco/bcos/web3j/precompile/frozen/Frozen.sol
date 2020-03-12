27012d12746811bdd0045a4e0a952fc201f02287
chaychen2005 <605966652@qq.com>
2020-03-12 11:07:21 +0800

    contract status management(csm) implementation

--- src/main/java/org/fisco/bcos/web3j/precompile/frozen/Frozen.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.2;
-
-contract Frozen {
-    function kill(address addr) public returns(int);
-    function freeze(address addr) public returns(int);
-    function unfreeze(address addr) public returns(int);
-    function queryStatus(address addr) public constant returns(uint,string);
-    function queryAuthority(address addr) public constant returns(uint,address[]);
-}
