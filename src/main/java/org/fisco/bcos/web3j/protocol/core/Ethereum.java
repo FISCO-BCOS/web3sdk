6b2aafa42a680c9c7b5dd06d0824c58ca6cc7987
ywy2090 <912554887@qq.com>
2020-03-20 13:20:16 +0800

    add Revert message resolve function (#536)
    * add RevertResolver
    
    * add RevertResolver UT

--- src/main/java/org/fisco/bcos/web3j/protocol/core/Ethereum.java
+++ src/main/java/org/fisco/bcos/web3j/protocol/core/Ethereum.java
@@ -1,7 +1,9 @@
 package org.fisco.bcos.web3j.protocol.core;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.List;
+import org.fisco.bcos.channel.client.TransactionSucCallback;
 import org.fisco.bcos.web3j.protocol.core.methods.response.BcosBlock;
 import org.fisco.bcos.web3j.protocol.core.methods.response.BcosFilter;
 import org.fisco.bcos.web3j.protocol.core.methods.response.BcosLog;
@@ -96,6 +98,9 @@ public interface Ethereum {
             DefaultBlockParameter defaultBlockParameter);
 
     Request<?, SendTransaction> sendRawTransaction(String signedTransactionData);
+
+    void sendRawTransaction(String signedTransactionData, TransactionSucCallback callback)
+            throws IOException;
 
     // generateGroup
     Request<?, GenerateGroup> generateGroup(int groupId, int timestamp, List<String> nodeList);
