ff6c118aac7bfed435c7ae38b73ded51d58946b7
octopus <912554887@qq.com>
2020-05-12 23:57:56 +0800

    add sm for integ test

--- .ci/ci_check.sh
+++ .ci/ci_check.sh
@@ -21,3 +21,23 @@ bash gradlew verifyGoogleJavaFormat
 bash gradlew build
 bash gradlew test
 bash gradlew integrationTest
+
+# clean
+bash nodes/127.0.0.1/stop_all.sh
+bash nodes/127.0.0.1/stop_all.sh
+bash nodes/127.0.0.1/stop_all.sh
+rm -rf nodes
+
+##
+bash build_chain.sh -e bin/fisco-bcos -g -f ipconf -p 30300,20200,8545
+bash nodes/127.0.0.1/start_all.sh
+ls -al nodes/127.0.0.1/sdk
+cp -r nodes/127.0.0.1/sdk/* src/integration-test/resources/
+cp src/test/resources/applicationContext-sample.xml src/integration-test/resources/applicationContext.xml
+sed -i.bak 's/"0"/"1"/g' src/integration-test/resources/applicationContext.xml
+cat src/integration-test/resources/applicationContext.xml
+cp src/test/resources/log4j.properties src/integration-test/resources/
+bash gradlew verifyGoogleJavaFormat
+bash gradlew build
+bash gradlew test
+bash gradlew integrationTest
