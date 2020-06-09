4eab73fbc4b41c16dd02b10d4e176d914c8249ba
bxq2011hust <bxq2011hust@qq.com>
2020-06-09 21:46:30 +0800

    update ci config

--- .ci/ci_check.sh
+++ .ci/ci_check.sh
@@ -11,8 +11,8 @@ bash gradlew build
 # UT test
 bash gradlew test
 
-curl -LO https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/release-2.5.0/tools/build_chain.sh && chmod u+x build_chain.sh
-curl -s https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/release-2.5.0/tools/ci/download_bin.sh | bash -s -- -m -b release-2.5.0
+curl -LO https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/master/tools/build_chain.sh && chmod u+x build_chain.sh
+curl -s https://raw.githubusercontent.com/FISCO-BCOS/FISCO-BCOS/master/tools/ci/download_bin.sh | bash -s -- -m -b master
 echo "127.0.0.1:4 agency1 1,2,3" > ipconf
 
 # bash build_chain.sh -h
